401f7b224e682f3e41611fc46a10d1893fb2af2e
Artur Sizov <artur-sa@mail.ru>
2023-12-12 16:53:16 +0300

    Начата работа над диалоговым окном добавления организации

--- Planner/Components/Layout/NavMenu.razor.cs
+++ Planner/Components/Layout/NavMenu.razor.cs
@@ -1,7 +1,19 @@
-﻿namespace Planner.Components.Layout
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Planner.Components.Dialogs;
+
+namespace Planner.Components.Layout
 {
     partial class NavMenu
     {
+        [Inject]
+        public IDialogService DialogService { get; set; } = new DialogService();
+
+        public void OpenDialog()
+        {
+            var options = new DialogOptions { CloseOnEscapeKey = true };
+            DialogService.Show<CreatingBranch>("Last element focused", options);
+        }
 
     }
 }
